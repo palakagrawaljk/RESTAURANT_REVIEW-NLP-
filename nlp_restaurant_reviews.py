41f26c92c090a40ce4fdc26b59454f480e41ca38
palakagrawaljk <35663855+palakagrawaljk@users.noreply.github.com>
2019-03-31 18:10:41 +0530

    Update nlp_restaurant_reviews.py

--- nlp_restaurant_reviews.py
+++ nlp_restaurant_reviews.py
@@ -1,46 +1,20 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
-
+#import all packages required
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-
-# In[ ]:
-
-
-data=pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
-
-
-# In[ ]:
-
-
-data
-
-
-# In[ ]:
-
-
 import re
 import nltk
 
+#use pandas to read tsv file , and tag is used as delimiter to distinguish both attributes.
+data=pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
 
-# In[ ]:
-
-
+#stemming is done by creating object
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps=PorterStemmer()
 
-
-# In[ ]:
-
-
+#data cleaning steps
 corp=[]
 for i in range(1000):
     review=re.sub('[^a-zA-Z]',' ',data['Review'][i])
@@ -50,64 +24,25 @@ for i in range(1000):
     review=' '.join(review)
     corp.append(review)
 
-
-# In[ ]:
-
-
-corp
-
-
-# In[ ]:
-
-
+#tokenization
 from sklearn.feature_extraction.text import CountVectorizer
 cv=CountVectorizer()
 
-
-# In[ ]:
-
-
+#creating a sparse matrix and converting to array
 x=cv.fit_transform(corp)
 y=data.iloc[:,1]
-y
-
-
-# In[ ]:
-
-
-x
-
-
-# In[ ]:
-
-
 X=x.toarray()
 
-
-# In[ ]:
-
-
-X
-
-
-# In[ ]:
-
-
+#seperation of training and testing data
 from sklearn.cross_validation import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 
-
-# In[ ]:
-
-
+#training the data
 from sklearn.naive_bayes import GaussianNB
 classifier=GaussianNB()
 classifier.fit(X_train,y_train)
 
-
-# In[ ]:
-
-
+#creating a confusion matrix
 y_pred=classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 confusion_matrix(y_test,y_pred)
